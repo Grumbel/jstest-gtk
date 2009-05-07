b3dcb6f6342fac0869ca723db670ed612d374a33
Ingo Ruhnke <grumbel@gmx.de>
2009-05-07 21:16:17 +0200

    Switched to Cairo for AxisWidget

--- src/axis_widget.cpp
+++ src/axis_widget.cpp
@@ -22,41 +22,93 @@ AxisWidget::AxisWidget(int width, int height)
   : x(0), y(0)
 {
   set_size_request(width, height);
-  modify_bg(Gtk::STATE_NORMAL , Gdk::Color("white"));
-  modify_fg(Gtk::STATE_NORMAL , Gdk::Color("black"));
+  //modify_bg(Gtk::STATE_NORMAL , Gdk::Color("white"));
+  //modify_fg(Gtk::STATE_NORMAL , Gdk::Color("black"));
 }
 
 bool
 AxisWidget::on_expose_event(GdkEventExpose* event)
 {
-  get_window()->draw_rectangle(get_style()->get_fg_gc(get_state()),
-                               false,
-                               0, 0,
-                               get_allocation().get_width()-1, get_allocation().get_height()-1);
+  Glib::RefPtr<Gdk::Window> window = get_window();
+  if(window)
+    {
+      if (1) 
+        {
+          Cairo::RefPtr<Cairo::Context> cr = window->create_cairo_context();
+          if (0)
+            {
+              cr->rectangle(event->area.x, event->area.y,
+                            event->area.width, event->area.height);
+              cr->clip();
+            }
+          cr->set_source_rgb(0.0, 0.0, 0.0);
+          cr->set_line_width(2.0);
+          cr->rectangle(event->area.x, event->area.y,
+                        event->area.width, event->area.height);
+          cr->stroke();
 
-  int cx = get_allocation().get_width()/2;
-  int cy = get_allocation().get_height()/2;
+          int px = get_allocation().get_width()/2  + (get_allocation().get_width()/2  * x);
+          int py = get_allocation().get_height()/2 + (get_allocation().get_height()/2 * y);
+          int w  = get_allocation().get_width();
+          int h  = get_allocation().get_height();
 
-  get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
-                         false,
-                         0, 0,
-                         get_allocation().get_width()-1, get_allocation().get_height()-1,
-                         0, 64 * 360);
+          // BG Circle
+          cr->arc(w/2, h/2, w/2, 0.0, 2.0 * M_PI);
+          cr->set_source_rgba(0.0, 0.0, 0.0, 0.1);
+          cr->fill();
 
-  get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
-                         false,
-                         cx-4, cy-4,
-                         9, 9,
-                         0, 64 * 360);
+          // Cross
+          cr->set_line_width(0.5);
+          cr->set_source_rgba(0.0, 0.0, 0.0, 0.5);
+          cr->move_to(w/2, 0);
+          cr->line_to(w/2, h);
 
-  int px = get_allocation().get_width()/2  + (get_allocation().get_width()/2  * x);
-  int py = get_allocation().get_height()/2 + (get_allocation().get_height()/2 * y);
+          cr->set_source_rgba(0.0, 0.0, 0.0, 0.5);
+          cr->move_to(0, h/2);
+          cr->line_to(h, h/2);
+          cr->stroke();
 
-  get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
-                         true,
-                         px-5, py-5,
-                         11, 11,
-                         0, 64 * 360);
+          // Cursor
+          cr->set_source_rgb(0.0, 0.0, 0.0);
+          cr->set_line_width(2.0);
+          cr->move_to(px, py-5);          
+          cr->line_to(px, py+5);
+          cr->move_to(px-5, py);          
+          cr->line_to(px+5, py);
+          cr->stroke();
+        }
+      else
+        {
+          get_window()->draw_rectangle(get_style()->get_fg_gc(get_state()),
+                                       false,
+                                       0, 0,
+                                       get_allocation().get_width()-1, get_allocation().get_height()-1);
+
+          int cx = get_allocation().get_width()/2;
+          int cy = get_allocation().get_height()/2;
+
+          get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
+                                 false,
+                                 0, 0,
+                                 get_allocation().get_width()-1, get_allocation().get_height()-1,
+                                 0, 64 * 360);
+
+          get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
+                                 false,
+                                 cx-4, cy-4,
+                                 9, 9,
+                                 0, 64 * 360);
+
+          int px = get_allocation().get_width()/2  + (get_allocation().get_width()/2  * x);
+          int py = get_allocation().get_height()/2 + (get_allocation().get_height()/2 * y);
+
+          get_window()->draw_arc(get_style()->get_fg_gc(get_state()),
+                                 true,
+                                 px-5, py-5,
+                                 11, 11,
+                                 0, 64 * 360);
+        }
+    }
   
   return true;
 }
