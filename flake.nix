bb33481a9a648f3cbd800757b966eb6f9e5e6438
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 18:58:51 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple joystick tester based on Gtk+";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
